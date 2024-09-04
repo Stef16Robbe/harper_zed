e93046b547f598351b1f1a7df510639adf3a2ae3
Stef Robbe <stef.robbe@protonmail.com>
2024-09-04 13:21:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# harper_zed
---
+++ src/lib.rs
@@ -0,0 +1,131 @@
+// https://github.com/WeetHet/typst.zed
+use std::fs;
+use zed_extension_api::{self as zed, Result};
+
+struct HarperExtension {
+    cached_binary_path: Option<String>,
+}
+
+#[derive(Clone)]
+struct HarperBinary {
+    path: String,
+    environment: Option<Vec<(String, String)>>,
+}
+
+impl HarperExtension {
+    fn language_server_binary(
+        &mut self,
+        language_server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<HarperBinary> {
+        if let Some(path) = worktree.which("harper-ls") {
+            let env = worktree.shell_env();
+            return Ok(HarperBinary {
+                path,
+                environment: Some(env),
+            });
+        }
+
+        if let Some(path) = &self.cached_binary_path {
+            if fs::metadata(path).map_or(false, |stat| stat.is_file()) {
+                return Ok(HarperBinary {
+                    path: path.clone(),
+                    environment: None,
+                });
+            }
+        }
+
+        zed::set_language_server_installation_status(
+            language_server_id,
+            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
+        );
+        let release = zed::latest_github_release(
+            "elijah-potter/harper",
+            zed::GithubReleaseOptions {
+                require_assets: true,
+                pre_release: false,
+            },
+        )?;
+
+        let (platform, arch) = zed::current_platform();
+        let asset_name = format!(
+            "harper-ls-{arch}-{os}",
+            arch = match arch {
+                zed::Architecture::Aarch64 => "aarch64",
+                zed::Architecture::X8664 => "x86_64",
+                zed::Architecture::X86 => return Err("Does not support x86".to_string()),
+            },
+            os = match platform {
+                zed::Os::Mac => "apple-darwin",
+                zed::Os::Linux => "unknown-linux-gnu",
+                zed::Os::Windows => "pc-windows",
+            },
+        );
+
+        let asset = release
+            .assets
+            .iter()
+            .find(|asset| asset.name == asset_name)
+            .ok_or_else(|| format!("no asset found matching {:?}", asset_name))?;
+
+        let version_dir = format!("harper-ls-{}", release.version);
+        fs::create_dir_all(&version_dir).map_err(|e| format!("failed to create directory: {e}"))?;
+
+        let binary_path = format!("{version_dir}/harper-ls");
+
+        if !fs::metadata(&binary_path).map_or(false, |stat| stat.is_file()) {
+            zed::set_language_server_installation_status(
+                language_server_id,
+                &zed::LanguageServerInstallationStatus::Downloading,
+            );
+
+            zed::download_file(
+                &asset.download_url,
+                &binary_path,
+                zed::DownloadedFileType::GzipTar,
+            )
+            .map_err(|e| format!("failed to download file: {e}"))?;
+
+            zed::make_file_executable(&binary_path)?;
+
+            let entries =
+                fs::read_dir(".").map_err(|e| format!("failed to list working directory {e}"))?;
+            for entry in entries {
+                let entry = entry.map_err(|e| format!("failed to load directory entry {e}"))?;
+                if entry.file_name().to_str() != Some(&version_dir) {
+                    fs::remove_dir_all(&entry.path()).ok();
+                }
+            }
+        }
+
+        self.cached_binary_path = Some(binary_path.clone());
+        Ok(HarperBinary {
+            path: binary_path,
+            environment: None,
+        })
+    }
+}
+
+// the the comment checking for speling errors
+impl zed::Extension for HarperExtension {
+    fn new() -> Self {
+        Self {
+            cached_binary_path: None,
+        }
+    }
+
+    fn language_server_command(
+        &mut self,
+        language_server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        let harper_binary = self.language_server_binary(language_server_id, worktree)?;
+        Ok(zed::Command {
+            command: harper_binary.path,
+            args: vec![],
+            env: harper_binary.environment.unwrap_or_default(),
+        })
+    }
+}
+
+zed::register_extension!(HarperExtension);
